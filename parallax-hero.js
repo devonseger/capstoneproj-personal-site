9288fc41452cd9ef76cea8382e722d9b66a821a9
devonseger <153763968+devonseger@users.noreply.github.com>
2023-12-19 17:45:48 -0600

    Add files via upload

---
+++ parallax-hero.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(window).scroll(function(e){
+      parallax();
+    });
+    function parallax(){
+      var scrolled = $(window).scrollTop();
+      $('.hero').css('top',-(scrolled*0.0315)+'rem');
+      $('.hero > h1').css('top',-(scrolled*-0.005)+'rem');
+      $('.hero > h1').css('opacity',1-(scrolled*.00175));
+      $('.hero > h3').css('top',-(scrolled*-0.005)+'rem');
+      $('.hero > h3').css('opacity',1-(scrolled*.00175));
+    };
+  });
